fda15daba3061c3034930138c33351734e69ecb5
Dharmen Shah <shhdharmen@gmail.com>
2024-01-26 17:55:17 +0530

    fix: for stackblitz

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+// needed for stackblitz
+import 'zone.js';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
